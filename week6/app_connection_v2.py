ded31388fbf9d31cd29e7ad5e9a3c0e1d845294f
TWjohnwang <108091299+TWjohnwang@users.noreply.github.com>
2022-10-29 03:32:35 +0800

    Update app_connection_v2.py

--- week6/app_connection_v2.py
+++ week6/app_connection_v2.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, redirect, url_for, render_template, session
-import mysql.connector
 from mysql.connector.pooling import MySQLConnectionPool
 
 connection = MySQLConnectionPool(user='root',
